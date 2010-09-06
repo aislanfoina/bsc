a9d44a81bf8edce375f5c627673c108891da662c
al-nisr <>
2010-09-06 09:47:09 +0000

    Fix no segfault e no problema da divergencia de resultados. Codigo limpo, com possibilidades de alguma optimizacao.

--- Encoplot/encoplot.c
+++ Encoplot/encoplot.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include "math.h"
 
-#define insidetasks
+#define BENCHMARK
 
 #define BLOCK_RECORDS 500000
 
@@ -49,114 +49,6 @@ double maintime_int(int print) {
 }
 
 
-
-
-
-
-//task1 - independent - input NN inout inputArray
-#pragma css task input (size, offset) \
-                 output (array[size])
-
-void init_inputArray_task(t_int *array, int size, int offset) {
-	int i;
-	for(i=0;i < size; i++)
-		array[i] = i+offset;
-}
-
-//task2 - independent - input RANGE inout counters
-#pragma css task input (size, offset) \
-	             output (counters[size])
-
-void init_counters_task (t_int *counters, int size, int offset) {
-	int k;
-	for(k=0; k < size; k++)
-		counters[k] = 0;
-}
-
-//task3 histogram - reduction - input NN, buffer inout counters
-#pragma css task input (size, offset, cntsize, bufsize, buffer[bufsize]) inout (counters[cntsize]) reduction (counters[cntsize])
-
-void counters_load_task(t_int *counters, int cntsize, unsigned char *buffer, int bufsize, int size, int offset) {
-	int i;
-
-	t_int localcounters[cntsize];
-	memset(localcounters, 0, cntsize * sizeof(t_int));
-
-	for(i=offset; i < size; i++) {
-		localcounters[*(buffer + i)]++;
-	}
-#pragma css mutex lock (counters)
-	for(i=0; i < cntsize; i++)
-		counters[i] += localcounters[i];
-#pragma css mutex unlock (counters)
-
-}
-
-//task4 - nonparallel
-#pragma css task input (size, counters[size]) \
-	             output (startpos[size])
-
-void init_startpos_task(t_int *startpos, t_int *counters, int size) {
-	t_int sp = 0;
-	int k;
-
-	for(k = 0; k < size; k++) {
-		startpos[k] = sp;
-		sp += counters[k];
-	}
-}
-/*
-//task 5 major loop
-#pragma css task input (size, ofs, bufsize, OFFSET, CPYCHAR, NUMCPYCHAR) \
-				 input (buffer[bufsize], inputArray[size]) \
-	             output (outputArray[NUMCPYCHAR])
-
-void load_outputArray_task(t_int *inputArray, t_int *outputArray, unsigned char *buffer, int bufsize, int size, int ofs, int OFFSET, unsigned char CPYCHAR, int NUMCPYCHAR) {
-	int i;
-	int offset_counter = 0;
-	int copy_counter = 0;
-
-	for(i = 0; i < size; i++) {
-		unsigned char c = buffer[ofs + inputArray[i]];
-		if (c == CPYCHAR) {
-			if(offset_counter < OFFSET)
-				offset_counter++;
-			else {
-				outputArray[copy_counter] = inputArray[i];
-				copy_counter++;
-				if(copy_counter == NUMCPYCHAR)
-					break;
-			}
-		}
-	}
-}
-*/
-
-//task 5 major loop
-#pragma css task input (size, ofs, bufsize, OFFSET, CPYCHAR, NUMCPYCHAR) \
-				 input (buffer[bufsize], inputArray[size]) \
-	             output (outputArray[NUMCPYCHAR])
-
-void load_outputArray_task(t_int *inputArray, t_int *outputArray, unsigned char *buffer, int bufsize, int size, int ofs, int OFFSET, unsigned char CPYCHAR, int NUMCPYCHAR) {
-	int i;
-	int offset_counter = 0;
-	int copy_counter = 0;
-
-	for(i = 0; i < size; i++) {
-		unsigned char c = buffer[ofs + inputArray[i]];
-		if (c == CPYCHAR) {
-			if(offset_counter < OFFSET)
-				offset_counter++;
-			else {
-				outputArray[copy_counter] = inputArray[i];
-				copy_counter++;
-				if(copy_counter == NUMCPYCHAR)
-					break;
-			}
-		}
-	}
-}
-
 #pragma css task input (SIZE, typeSize, orig[SIZE]) \
                  output (dst[SIZE])
 
@@ -173,15 +65,6 @@ word_t readat(unsigned char *buf,int poz)
 	return rez;
 }
 
-int compare_number (const word_t *a, const word_t *b) {
-	if (*a > *b)
-		return 1;
-	else
-		return -1;
-
-	return 0;
-}
-
 int compareELM (const ELM *a, const ELM *b) {
 	if(a->ngram < b->ngram)
 		return -1;
@@ -195,7 +78,6 @@ int compareELM (const ELM *a, const ELM *b) {
 			return 1;
 	}
 }
-
 
 
 #pragma css task input (size, offset, depth, buffer[size+depth]) \
@@ -225,44 +107,22 @@ void simpler_qsort4ngrams(unsigned char *buffer, int numlines, int DEPTH, t_int 
 	int NN = numlines - DEPTH + 1;
 	int i;
 
-//	ELM *array;
-//	array = malloc(NN * sizeof(ELM));
-
-/*	ELM *array2;
-	array2 = malloc(NN * sizeof(ELM));
-
-	word_t *array3;
-	array3 = malloc(NN * sizeof(word_t));*/
-
 	for (i = 0; i < NN; i += block_records) {
 		chunk_of_records = (i + block_records > NN ? NN - i : block_records);
 		init_ELMArray_task(&array[i], &buffer[i], chunk_of_records, DEPTH, i);
 	 }
-/*
-	for (i = 0; i < NN; i++) {
-		array[i].ngram = readat(buffer, i);
-		array[i].index = i;
-
-		array2[i].ngram = readat(buffer, i);
-		array2[i].index = i;
-
-		array3[i] = array[i].ngram;
-	}*/
-
-//	psort(array2, NN);
-//	qsort(array2, NN, sizeof(ELM),compareELM);
 
 #pragma css barrier
 
 	pngramsort(array, NN);
-/*
-	for (i = 0; i < NN; i++) {
-		index[i] = array[i].index;
-	}*/
-	for (i = 0; i < NN; i += block_records) {
+
+// For compatibility porpouse...
+
+/*	for (i = 0; i < NN; i += block_records) {
 		chunk_of_records = (i + block_records > NN ? NN - i : block_records);
 		copy_ELMArray_task(&array[i], &index[i], chunk_of_records);
 	 }
+*/
 
 }
 
@@ -328,180 +188,6 @@ void simpler_rsort4ngrams_orig(unsigned char *buffer, int numlines, int DEPTH, i
 }
 
 
-
-void simpler_rsort4ngrams(unsigned char *buffer, int numlines, int DEPTH, int *index, int *counter_output, int *startpos_output) {
-	const int RANGE = 256;
-
-	int a, b, c;
-	int i, j, k;
-
-//	int big_block_records = 60;
-	int big_block_records = 20000000;
-	int block_records = 500000000;
-	int chunk_of_records = 0;
-
-	if(numlines > big_block_records+DEPTH) {
-		int pages = (int)ceil((float)numlines/(float)big_block_records);
-		t_int **index_pages;
-		t_int **counter_pages;
-		t_int **startpos_pages;
-
-		index_pages = malloc(pages*sizeof(t_int*));
-		counter_pages = malloc(pages*sizeof(t_int*));
-		startpos_pages = malloc(pages*sizeof(t_int*));
-		b = 0;
-		for (a = 0; a < numlines; a += big_block_records) {
-			int chunk_of_records = (a + big_block_records > numlines ? numlines - a : big_block_records + DEPTH - 1);
-			index_pages[b] = (t_int*) malloc(chunk_of_records * sizeof(t_int));
-			counter_pages[b] = (t_int*) malloc(RANGE * sizeof(t_int));
-			startpos_pages[b] = (t_int*) malloc(RANGE * sizeof(t_int));
-
-			simpler_rsort4ngrams(&buffer[a], chunk_of_records, DEPTH, index_pages[b], counter_pages[b], startpos_pages[b]);
-
-			if(a > 0) {
-				for(c = 0; c < chunk_of_records; c++)
-					index_pages[b][c] += a;
-			}
-			b++;
-		}
-//		printf("Merge\n");
-		int NN = numlines - DEPTH + 1;
-		t_int *inputArray = (t_int*) malloc(NN * sizeof(t_int));
-
-		t_int counters[RANGE];
-		t_int startpos[RANGE];
-
-		//task2 - independent - input RANGE inout counters
-		init_counters_task(counters, RANGE, 0);
-
-		//task3 histogram - reduction - input NN, buffer inout counters
-
-		for (k = 0; k < NN; k += block_records) {
-			chunk_of_records = (k + block_records > NN ? NN - k : block_records);
-			counters_load_task(counters, RANGE, buffer, numlines, k+chunk_of_records, k);
-		}
-
-		unsigned char *pin = buffer + NN;
-		unsigned char *pout = buffer;
-		for (j = 0; j < DEPTH - 1; j++) {
-			counters[*pout++]--;
-			counters[*pin++]++;
-		}
-
-		//task 4
-		init_startpos_task(startpos, counters, RANGE);
-
-		for (i = 0; i < RANGE; i++) {
-			int offset = 0;
-			for (j = 0; j < pages; j++) {
-				if(counter_pages[j][i] > 0) {
-//					printf("i = %d, j = %d, startpos[i] = %d, offset = %d\n", i, j, startpos[i], offset);
-//					printf("startpos_pages[j][i] = %d, index_pages[j][startpos_pages[j][i]] = %d, counter_pages[j][i] = %d\n",startpos_pages[j][i],index_pages[j][startpos_pages[j][i]], counter_pages[j][i]);
-//					printf("startpos[i+1] = %d, startpos_pages[j][i+1] = %d, counter_pages[j][i+1] = %d\n", startpos[i+1], startpos_pages[j][i+1], counter_pages[j][i+1]);
-				}
-				memcpy_task(&index[startpos[i]+offset], &index_pages[j][startpos_pages[j][i]], counter_pages[j][i], sizeof(t_int));
-				if(counter_pages[j][i] > 0) {
-//					printf("Copied!\n");
-				}
-				offset += counter_pages[j][i];
-			}
-		}
-		return;
-	}
-	else {
-
-	int NN = numlines - DEPTH + 1;
-
-	if (NN > 0) {
-		unsigned char *pin = buffer + NN;
-		unsigned char *pout = buffer;
-		typedef int t_int;
-		t_int *inputArray = (t_int*) malloc(NN * sizeof(t_int));
-		t_int *outputArray = (t_int*) malloc(NN * sizeof(t_int));
-		t_int counters[RANGE];
-		t_int startpos[RANGE];
-
-
-
-
-
-
-
-		//task1 - independent - input NN inout inputArray
-		  for (k = 0; k < NN; k += block_records) {
-  			chunk_of_records = (k + block_records > NN ? NN - k : block_records);
-			init_inputArray_task(&inputArray[k], chunk_of_records, k);
-		  }
-
-
-		//radix sort, the input is x, the output rank is ix
-		//counters
-
-
-		//task2 - independent - input RANGE inout counters
-		init_counters_task(counters, RANGE, 0);
-
-
-		//task3 histogram - reduction - input NN, buffer inout counters
-
-		for (k = 0; k < NN; k += block_records) {
-			chunk_of_records = (k + block_records > NN ? NN - k : block_records);
-			counters_load_task(counters, RANGE, buffer, numlines, k+chunk_of_records, k);
-		}
-
-		for(j=0; j < DEPTH; j++) {
-			int ofs = j;//low endian
-			t_int sp = 0;
-
-
-			//task 4
-			init_startpos_task(startpos, counters, RANGE);
-
-			//task 5 major loop - task reduction
-
-#pragma css barrier
-
-			for (i = 0; i < RANGE; i++) {
-				for(k = 0; k < counters[i]; k+= block_records) {
-					chunk_of_records = (k + block_records > counters[i] ? counters[i] - k : block_records);
-					load_outputArray_task(inputArray, &outputArray[startpos[i]+k], buffer, numlines, NN, ofs, k, i, chunk_of_records);
-				}
-			}
-
-#pragma css barrier
-
-			for (k = 0; k < NN; k += block_records) {
-				chunk_of_records = (k + block_records > NN ? NN - k : block_records);
-				memcpy_task(&inputArray[k], &outputArray[k], chunk_of_records, sizeof(inputArray[0]));
-			}
-
-#pragma css barrier
-
-			//update counters
-			if (j < DEPTH - 1) {
-				counters[*pout++]--;
-				counters[*pin++]++;
-			}
-
-		}
-
-#pragma css wait on (inputArray)
-
-		memcpy_task(counter_output, counters, RANGE, sizeof(t_int));
-		memcpy_task(startpos_output, startpos, RANGE, sizeof(t_int));
-
-		for (k = 0; k < NN; k += block_records) {
-			chunk_of_records = (k + block_records > NN ? NN - k : block_records);
-			memcpy_task(&index[k], &inputArray[k], chunk_of_records, sizeof(inputArray[0]));
-		}
-
-		free(outputArray);
-
-	} else
-		index = NULL;
-	}
-}
-
 #define MAXBUFSIZ 8000123
 unsigned char bufferfile1[MAXBUFSIZ];
 unsigned char bufferfile2[MAXBUFSIZ];
@@ -517,7 +203,7 @@ int isELMequal (ELM *a, ELM *b) {
 
 #pragma css task input (size, num_idx) \
                  output (index[num_idx])
-//void startidx(ELM *array, int size, t_int *index, int num_idx) {
+
 void startidx(int size, t_int *index, int num_idx) {
 	int i;
 	int num = 0;
@@ -528,7 +214,6 @@ void startidx(int size, t_int *index, int num_idx) {
 		index[num] = (t_int) i;
 		num++;
 	}
-//	*num_idx = num;
 }
 
 #pragma css task input (numlines1, numlines2, idx_ar1, idx_ar2, array1[numlines1], array2[numlines2], startVal, endVal) \
@@ -543,19 +228,16 @@ void compareArray(ELM *array1, int numlines1, t_int idx_ar1, ELM *array2, int nu
 	ELM *ar1Last;
 	ELM *ar2Last;
 
-//	int linecnt1 = idx_ar1;
-//	int linecnt2 = idx_ar2;
-
 	tmp1 = &array1[idx_ar1];
 	tmp2 = &array2[idx_ar2];
 
 	ar1Last = &array1[numlines1-1];
 	ar2Last = &array2[numlines2-1];
 
-	while (startVal >= tmp2->ngram) {
-		tmp2++;
-//		linecnt2++;
-//		tmp2 = array2[linecnt2];
+	if(!isELMequal(tmp1,&array1[0])) {
+		while (startVal >= tmp2->ngram && !isELMequal(tmp2,ar2Last)) {
+			tmp2++;
+		}
 	}
 
 	int stop = 0;
@@ -592,29 +274,6 @@ void compareArray(ELM *array1, int numlines1, t_int idx_ar1, ELM *array2, int nu
 		}
 	}
 	*cnt = tmp_cnt;
-	/*
-	while (linecnt1 < numlines1 && linecnt2 < numlines2) {
-		//		fprintf(stderr,"i1=%d i2=%d s1=%0llx s2=%0llx\n",i1,i2,s1,s2);
-		if (str1 == str2) {
-//				printf("%d %d\n", index1[linecnt1], index2[linecnt2]);
-			cnt++;
-			linecnt1++;
-			if (linecnt1 < numlines1)
-				str1 = readat(bufferfile1, index1[linecnt1]);
-			linecnt2++;
-			if (linecnt2 < numlines2)
-				str2 = readat(bufferfile2, index2[linecnt2]);
-		} else if (str1 < str2) {
-			linecnt1++;
-			if (linecnt1 < numlines1)
-				str1 = readat(bufferfile1, index1[linecnt1]);
-		} else if (str2 < str1) {
-			linecnt2++;
-			if (linecnt2 < numlines2)
-				str2 = readat(bufferfile2, index2[linecnt2]);
-		}
-	}
-*/
 }
 
 
@@ -673,21 +332,16 @@ void compare2files(ELM *array1, int numlines1, ELM *array2, int numlines2, int d
 		}
 		else {
 			compareArray(array1,numlines1,index_ar1[cnt1], array2, numlines2, index_ar2[cnt2-1], startVal, endVal, &cnt[cnt1]);
-//			compareArray(&array1[index_ar1[cnt1]], &array2[index_ar2[cnt2-1]], startVal, endVal, &cnt[cnt1]);
 		}
 	}
+
+#pragma css barrier
+
 	int total = 0;
 	for(i = 0; i < cnt1; i++)
 		total += cnt[i];
 	printf("++++ cnt = %d\n",total);
 }
-
-
-/*
-inline word_t readat(const unsigned char *buf, int poz) {
-	return *(word_t *) (buf + poz);
-}
-*/
 
 int main(int argc, char ** argv) {
 	int depth = sizeof(word_t);
@@ -710,14 +364,25 @@ int main(int argc, char ** argv) {
 
 
 	t_int *index1 = (t_int*) malloc(numlines1 * sizeof(t_int));
-	t_int *counter1 = (t_int*) malloc(256 * sizeof(t_int));
-	t_int *startpos1 = (t_int*) malloc(256 * sizeof(t_int));
 	t_int *index2 = (t_int*) malloc(numlines2 * sizeof(t_int));
-	t_int *counter2 = (t_int*) malloc(256 * sizeof(t_int));
-	t_int *startpos2 = (t_int*) malloc(256 * sizeof(t_int));
 
 	maintime_int(0);
-	printf("Original\n");
+	printf("New sort\n");
+
+	simpler_qsort4ngrams(bufferfile1, numlines1, depth, index1, array1);
+	simpler_qsort4ngrams(bufferfile2, numlines2, depth, index2, array2);
+
+#pragma css barrier
+
+	maintime_int(1);
+	printf("New comparing\n");
+
+	compare2files(array1, numlines1, array2, numlines2, depth);
+#pragma css barrier
+
+#ifdef BENCHMARK
+	maintime_int(1);
+	printf("Original sort\n");
 
 	simpler_rsort4ngrams_orig(bufferfile1, numlines1, depth, index1);
 	simpler_rsort4ngrams_orig(bufferfile2, numlines2, depth, index2);
@@ -725,23 +390,7 @@ int main(int argc, char ** argv) {
 #pragma css barrier
 
 	maintime_int(1);
-	printf("Novo\n");
-
-	simpler_qsort4ngrams(bufferfile1, numlines1, depth, index1, array1);
-	simpler_qsort4ngrams(bufferfile2, numlines2, depth, index2, array2);
-//	simpler_rsort4ngrams(bufferfile1, numlines1, depth, index1, counter1, startpos1);
-//	simpler_rsort4ngrams(bufferfile2, numlines2, depth, index2, counter2, startpos2);
-
-#pragma css barrier
-
-	maintime_int(1);
-printf("Comparacao Nova\n");
-
-	compare2files(array1, numlines1, array2, numlines2, depth);
-#pragma css barrier
-
-	maintime_int(1);
-	printf("Comparacao Antiga\n");
+	printf("Original comparing\n");
 
 	//merge
 	int linecnt1 = 0;
@@ -773,11 +422,12 @@ printf("Comparacao Nova\n");
 					str2 = readat(bufferfile2, index2[linecnt2]);
 			}
 		}
-		free(index2);
-		free(index1);
+//		free(index2);
+//		free(index1);
 	}
-#pragma css finish
+	printf(">>>> cnt = %d\n\n", cnt);
+#endif
 	maintime_int(1);
-printf(">>>> cnt = %d\n\n", cnt);
+#pragma css finish
 	return 0;
 }
